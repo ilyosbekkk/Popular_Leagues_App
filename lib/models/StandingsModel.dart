9b43c1a542c84b2e5ae43479aa593670c28d66cf
Ilyosbek Ibrokhimov <46449085+ilyosbekibrohimov@users.noreply.github.com>
2021-05-16 01:21:35 +0900

    Retrieve standings partially done

--- lib/models/StandingsModel.dart
+++ lib/models/StandingsModel.dart
@@ -1,24 +1,75 @@
 
 
 
-class StandingsModel{
+class StandingsModel {
   String get = "";
-  int resultsd  = -1;
+  int results = -1;
+  List<Response> response = [];
+
+  StandingsModel({required this.get, required this.results, required this.response});
 
 
 
 
+  factory StandingsModel.fromJson(Map<String,  dynamic> parsedJson) {
+    var response= parsedJson["response"] as List;
+    List<Response> reponses = response.map((e) => Response.fromJson(e)).toList();
+    return StandingsModel(get: parsedJson["get"], results:parsedJson["results"], response: reponses);
+
+  }
+}
+
+class Response {
+  League league = League(id: -1, name: "", country: "", logo_url: "", flag_url: "", season: "", standingds: []);
+
+  Response({required this.league});
+
+  factory  Response.fromJson(Map<String,  dynamic> parsedJson){
+    return Response(league: parsedJson["league"]);
+  }
 
 
 }
 
+class Standing {
+  List<Rank> ranks = [];
 
-class League{
+  Standing({required List<Rank> ranks});
+}
 
+class Rank {
+  String rank = "";
+  int points = -1;
+  int goalsDiff = 47;
+  String group = "";
+  Team team = Team(id: -1, name: "", logo_url: "");
+}
 
+class GameStats {
+  int played = -1;
+  int win = -1;
+  int draw = -1;
+  int lose = -1;
 
+  GameStats({required this.played, required this.win, required this.draw, required this.lose});
+}
 
+class Team {
+  int id = -1;
+  String name = "";
+  String logo_url = "";
 
+  Team({required this.id, required this.name, required this.logo_url});
+}
 
+class League {
+  int id = -1;
+  String name = "";
+  String country = "";
+  String logo_url = "";
+  String flag_url = "";
+  String season = "";
+  List<Standing> standingds = [];
 
+  League({required this.id, required this.name, required this.country, required this.logo_url, required this.flag_url, required this.season, required this.standingds});
 }
--- lib/providers/StandingsProvider.dart
+++ lib/providers/StandingsProvider.dart
@@ -1,12 +1,34 @@
 
 
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:flutter_provider/models/PlayersModel.dart';
+import 'package:flutter_provider/models/StandingsModel.dart';
+import 'package:flutter_provider/utils/Strings_English.dart';
+import 'package:http/http.dart' as  http;
 
 class StandingsProvider extends ChangeNotifier{
 
 
 
 
+
+
+  Future<void> retrieveStandings(int league_id)async {
+    var url= Uri.parse("${Strings.url}standings?league=$league_id&season=2020");
+    Map<String, String> headers = {Strings.apiKey1.keys.single: Strings.apiKey1.values.single, Strings.hostUrl.keys.single: Strings.hostUrl.values.single};
+    http.Response response = await http.get(url, headers: headers);
+    final jsonResponse = json.decode(response.body);
+
+    StandingsModel standingsModel  = StandingsModel.fromJson(jsonResponse);
+
+    print(standingsModel.response[0]);
+
+
+    notifyListeners();
+
+  }
 
 
 
